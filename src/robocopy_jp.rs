3351521c06ad5ce085814063b1c801b5aef7a921
jyuch <jyuch@users.noreply.github.com>
2022-02-15 22:18:08 +0900

    procedural parser

--- src/robocopy_jp.rs
+++ src/robocopy_jp.rs
@@ -1,53 +1,39 @@
-use chrono::{DateTime, Local, ParseResult, TimeZone};
+use chrono::{DateTime, Local, TimeZone};
 use nom::bytes::complete::tag;
 use nom::character::complete::{digit1, space0, space1};
 use nom::combinator::{map, map_res};
 use nom::{branch::permutation, IResult};
-
-#[allow(dead_code)]
-fn to_datetime(input: &str) -> ParseResult<DateTime<Local>> {
-    Local.datetime_from_str(input, "%Y年%m月%d日 %H:%M:%S")
-}
+use std::num::ParseIntError;
 
 #[allow(dead_code)]
 fn datetime(input: &str) -> IResult<&str, DateTime<Local>> {
-    map_res(
-        permutation((
-            digit1,
-            tag("年"),
-            digit1,
-            tag("月"),
-            digit1,
-            tag("日"),
-            space1,
-            digit1,
-            tag(":"),
-            digit1,
-            tag(":"),
-            digit1,
-        )),
-        |(s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11, s12): (
-            &str,
-            &str,
-            &str,
-            &str,
-            &str,
-            &str,
-            &str,
-            &str,
-            &str,
-            &str,
-            &str,
-            &str,
-        )|
-         -> ParseResult<DateTime<Local>> {
-            let s = format!(
-                "{}{}{}{}{}{}{}{}{}{}{}{}",
-                s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11, s12
-            );
-            to_datetime(s.as_str())
-        },
-    )(input)
+    let (input, year) = map_res(digit1, |it: &str| -> Result<i32, ParseIntError> {
+        it.parse()
+    })(input)?;
+    let (input, _) = tag("年")(input)?;
+    let (input, month) = map_res(digit1, |it: &str| -> Result<u32, ParseIntError> {
+        it.parse()
+    })(input)?;
+    let (input, _) = tag("月")(input)?;
+    let (input, day) = map_res(digit1, |it: &str| -> Result<u32, ParseIntError> {
+        it.parse()
+    })(input)?;
+    let (input, _) = tag("日")(input)?;
+    let (input, _) = space1(input)?;
+    let (input, h) = map_res(digit1, |it: &str| -> Result<u32, ParseIntError> {
+        it.parse()
+    })(input)?;
+    let (input, _) = tag(":")(input)?;
+    let (input, m) = map_res(digit1, |it: &str| -> Result<u32, ParseIntError> {
+        it.parse()
+    })(input)?;
+    let (input, _) = tag(":")(input)?;
+    let (input, s) = map_res(digit1, |it: &str| -> Result<u32, ParseIntError> {
+        it.parse()
+    })(input)?;
+
+    let d = Local.ymd(year, month, day).and_hms(h, m, s);
+    Ok((input, d))
 }
 
 #[allow(dead_code)]
@@ -56,13 +42,6 @@ fn start_datetime(input: &str) -> IResult<&str, DateTime<Local>> {
         permutation((tag("開始"), space0, tag(":"), space0, datetime)),
         |(_, _, _, _, d): (&str, &str, &str, &str, DateTime<Local>)| -> DateTime<Local> { d },
     )(input)
-}
-
-#[test]
-fn to_datetime_can_parse_robocopy_datetime_format() {
-    let expected = Local.ymd(2022, 2, 12).and_hms(19, 58, 39);
-    let actual = to_datetime("2022年2月12日 19:58:39").unwrap();
-    assert_eq!(expected, actual);
 }
 
 #[test]
